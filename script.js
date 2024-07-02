acecbf10fa8d9cd4e2928a44017aaaef21faa819
Kritarat.M <ideal141148@gmail.com>
2024-07-02 12:33:46 +0700

    add: more sections with animations

---
+++ script.js
@@ -0,0 +1,25 @@
+$(window).scroll(function () { 
+    $("#banner-scroll").css("transform", "translateX(" + ($(window).scrollTop()*0.3 - 500) + "px");
+});
+
+const infiniteLogo = $("#infinite-logo");
+const slideTime = 40;
+const slideItems = $(infiniteLogo).children();
+const ItemCount = $(infiniteLogo).children().length;
+const slideItemsGap = slideTime / ItemCount;
+
+slideItems.each( (index, item) => {
+    $(item).css("animation-delay", index * slideItemsGap - slideTime + "s");
+    $(item).css("animation-duration", slideTime + "s");
+})
+
+$(window).scroll(function () { 
+    $(".slideIn").each(function (index, item) {
+            const winScroll = $(window).scrollTop() + $(window).height();
+            const top = $(item).offset().top-100;
+            const FadeInPos = 50;
+            if(top+FadeInPos<=winScroll){
+                $(item).removeClass("slideIn");
+            }
+    });
+});
